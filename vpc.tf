244254909f255334f4afc6a4fd065422b74f4ce8
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-10-25 23:12:20 +0900

    subnet追加

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,26 @@ resource "aws_vpc" "mcs-vpc" {
     Name = "${var.resource_name_prefix}-mcs-vpc"
   }
 }
+
+resource "aws_subnet" "mcs-subnet-a" {
+  vpc_id                  = aws_vpc.mcs-vpc.id
+  cidr_block              = var.public_subnet_a_cidr_block
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.resource_name_prefix}-mcs-public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "mcs-subnet-c" {
+  vpc_id                  = aws_vpc.mcs-vpc.id
+  cidr_block              = var.public_subnet_c_cidr_block
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.resource_name_prefix}-mcs-public-subnet-c"
+  }
+}
+
