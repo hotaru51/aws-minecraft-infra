e6102275e11927c788a8fe310251d1674423abe6
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-10-25 23:46:32 +0900

    internet gatewayへのルート作成

--- vpc.tf
+++ vpc.tf
@@ -37,3 +37,26 @@ resource "aws_internet_gateway" "mcs-igw" {
     Name = "${var.resource_name_prefix}-mcs-igw"
   }
 }
+
+resource "aws_route_table" "mcs-public-rtb" {
+  vpc_id = aws_vpc.mcs-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mcs-igw.id
+  }
+
+  tags = {
+    Name = "${var.resource_name_prefix}-mcs-public-rtb"
+  }
+}
+
+resource "aws_route_table_association" "mcs-public-a-assoc" {
+  subnet_id      = aws_subnet.mcs-subnet-a.id
+  route_table_id = aws_route_table.mcs-public-rtb.id
+}
+
+resource "aws_route_table_association" "mcs-public-c-assoc" {
+  subnet_id      = aws_subnet.mcs-subnet-c.id
+  route_table_id = aws_route_table.mcs-public-rtb.id
+}
