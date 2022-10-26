c18b70b263a8e9fee2290c5560e053cb48a7f3c0
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-10-26 23:37:32 +0900

    インスタンス追加

---
+++ ec2.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "mcs-instance" {
+  ami                         = var.mcs_ami_id
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.mcs-ec2-instance-profile.name
+  instance_type               = var.mcs_instance_type
+  key_name                    = var.mcs_keypair
+  security_groups             = [aws_security_group.mcs-instance-sg.id]
+  subnet_id                   = aws_subnet.mcs-subnet-a.id
+
+  root_block_device {
+    volume_size = 16
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "${var.resource_name_prefix}-mcs-sv-01a"
+  }
+}
