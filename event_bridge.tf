e48c943871661122c7a2509d78407f3e13f74ac2
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-11-03 17:01:34 +0900

    DNSレコード操作用Lambdaの名前とイベント修正

--- event_bridge.tf
+++ event_bridge.tf
@@ -1,18 +1,18 @@
-resource "aws_cloudwatch_event_rule" "mcs-server-stop-event" {
-  name          = "${var.resource_name_prefix}-mcs-server-stop-event"
-  description   = "${var.resource_name_prefix}-mcs-server-stop-event"
+resource "aws_cloudwatch_event_rule" "mcs-instance-state-event" {
+  name          = "${var.resource_name_prefix}-mcs-instance-state-event"
+  description   = "${var.resource_name_prefix}-mcs-instance-state-event"
   event_pattern = <<EOF
 {
   "source": ["aws.ec2"],
   "detail-type": ["EC2 Instance State-change Notification"],
   "detail": {
-    "state": ["stopping"]
+    "state": ["stopping", "running"]
   }
 }
 EOF
 }
 
-resource "aws_cloudwatch_event_target" "mcs-server-stop-event-target" {
-  rule = aws_cloudwatch_event_rule.mcs-server-stop-event.name
-  arn  = aws_lambda_function.mcs-delete-dns-record-function.arn
+resource "aws_cloudwatch_event_target" "mcs-instance-state-event-target" {
+  rule = aws_cloudwatch_event_rule.mcs-instance-state-event.name
+  arn  = aws_lambda_function.mcs-register-dns-record-function.arn
 }
--- lambda.tf
+++ lambda.tf
@@ -1,16 +1,16 @@
-resource "aws_lambda_function" "mcs-delete-dns-record-function" {
-  filename         = "function_code/delete-dns-record-function.zip"
-  source_code_hash = "function_code/delete-dns-record-function.zip"
-  function_name    = "${var.resource_name_prefix}-mcs-delete-dns-record-function"
+resource "aws_lambda_function" "mcs-register-dns-record-function" {
+  filename         = "function_code/register-dns-record-function.zip"
+  source_code_hash = "function_code/register-dns-record-function.zip"
+  function_name    = "${var.resource_name_prefix}-mcs-register-dns-record-function"
   role             = aws_iam_role.mcs-function-role.arn
   handler          = "app.lambda_handler"
   runtime          = "ruby2.7"
 }
 
-resource "aws_lambda_permission" "mcs-allow-server-stop-event" {
+resource "aws_lambda_permission" "mcs-allow-instance-state-event" {
   statement_id  = "${var.resource_name_prefix}McsAllowServerStopEvent"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.mcs-delete-dns-record-function.function_name
+  function_name = aws_lambda_function.mcs-register-dns-record-function.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.mcs-server-stop-event.arn
+  source_arn    = aws_cloudwatch_event_rule.mcs-instance-state-event.arn
 }
