226a570bfc6d1063d0c33fc68f5181a2dad90763
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-11-10 22:59:43 +0900

    トークンの照合、インスタンスの起動/停止処理実装

--- function_code/instance-start-stop-function/app.rb
+++ function_code/instance-start-stop-function/app.rb
@@ -5,6 +5,7 @@ require 'json'
 require 'aws-sdk'
 
 require_relative 'lib/request_parser'
+require_relative 'lib/token_authenticator'
 
 def lambda_handler(event:, context:)
   logger = Logger.new(STDOUT)
@@ -19,12 +20,33 @@ def lambda_handler(event:, context:)
   # 有効なリクエストか確認する
   unless request.valid_path?
     logger.info("invalid path. (#{request.path}) do nothing.")
-    return { statusCode: 400, body: "invalid path. (#{request.path})" }
+    return { statusCode: 404, body: 'not found.' }
   end
 
   unless request.query_string.key?('t')
     logger.info('invalid query string. do nothing.')
-    return { statusCode: 400, body: 'invalid query string.' }
+    return { statusCode: 400, body: 'invalid request.' }
+  end
+
+  # トークンを照合する
+  token = request.query_string['t']
+  authenticator = TokenAuthenticator.new
+  unless authenticator.authentication(token)
+    logger.info("invalid token. (#{token}) do nothing.")
+    return { statusCode: 400, body: 'invalid token.' }
+  end
+
+  target_instance = ENV['TARGET_INSTANCE']
+  ec2_instance = Aws::EC2::Instance.new({ id: target_instance })
+  case request.path
+  when '/start'
+    # インスタンス起動
+    logger.info("start instance. (#{target_instance})")
+    ec2_instance.start
+  when '/stop'
+    logger.info("stop instance. (#{target_instance})")
+    # インスタンス停止
+    ec2_instance.stop
   end
 
   { statusCode: 200, body: 'ok' }
