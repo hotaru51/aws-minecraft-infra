88fb60f98f482446fe02760b9c1bf6ba1f26a087
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-11-27 21:27:06 +0900

    メンテナンスウィンドウ作成

--- ssm.tf
+++ ssm.tf
@@ -1,3 +1,11 @@
 data "aws_ssm_parameter" "mcs-token-parameter" {
   name = var.token_parameter_name
 }
+
+resource "aws_ssm_maintenance_window" "mcs-maintenance-window" {
+  name = "${var.resource_name_prefix}-mcs-daily-instance-stop"
+  schedule = "cron(0 2 * * ? *)"
+  schedule_timezone = "Asia/Tokyo"
+  duration = 1
+  cutoff = 0
+}
