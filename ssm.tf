172acca020f7ed74de8acb8856454117fe62bcc5
hotaru <36354847+hotaru51@users.noreply.github.com>
2022-11-28 23:37:17 +0900

    メンテナンスウィンドウのターゲット追加、コード整形

--- ssm.tf
+++ ssm.tf
@@ -3,9 +3,19 @@ data "aws_ssm_parameter" "mcs-token-parameter" {
 }
 
 resource "aws_ssm_maintenance_window" "mcs-maintenance-window" {
-  name = "${var.resource_name_prefix}-mcs-daily-instance-stop"
-  schedule = "cron(0 2 * * ? *)"
+  name              = "${var.resource_name_prefix}-mcs-daily-instance-stop"
+  schedule          = "cron(0 2 * * ? *)"
   schedule_timezone = "Asia/Tokyo"
-  duration = 1
-  cutoff = 0
+  duration          = 1
+  cutoff            = 0
+}
+
+resource "aws_ssm_maintenance_window_target" "mcs-maintenance-window-target" {
+  name          = "target-instance"
+  window_id     = aws_ssm_maintenance_window.mcs-maintenance-window.id
+  resource_type = "INSTANCE"
+  targets {
+    key    = "tag:InstanceGroup"
+    values = [var.resource_name_prefix]
+  }
 }
